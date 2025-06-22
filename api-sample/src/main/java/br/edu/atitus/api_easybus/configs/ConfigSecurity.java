f2fa304495c341194c40d8a845bc0d640f5f4615
Wagner Zanuzzi Schmitt <1137242@atitus.edu.br>
2025-06-22 18:50:55 -0300

    Adicionado novas configurações no CORS para permitir métodos http

--- api-sample/src/main/java/br/edu/atitus/api_easybus/configs/ConfigSecurity.java
+++ api-sample/src/main/java/br/edu/atitus/api_easybus/configs/ConfigSecurity.java
@@ -35,7 +35,11 @@ public class ConfigSecurity {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry
+				.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
 			}
 		};
 	}
