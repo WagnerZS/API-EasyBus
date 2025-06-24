35d47417c923751739f70978454cda7c0445ce3b
Wagner Zanuzzi Schmitt <1137242@atitus.edu.br>
2025-06-24 20:19:40 -0300

    Mudado pacote das classes para configuração do CORS

--- api-sample/src/main/java/br/edu/atitus/api_easybus/configs/ConfigSecurity.java
+++ api-sample/src/main/java/br/edu/atitus/api_easybus/configs/ConfigSecurity.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,10 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import br.edu.atitus.api_easybus.components.AuthTokenFilter;
 
